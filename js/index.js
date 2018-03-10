c95fdae07713f356048030026f98aeba64bfbc01
Yoshihiro Kimura <kimura.yoshihiro@tsunolab.net>
2018-03-10 23:33:23 +0900

    微修正等

--- js/index.js
+++ js/index.js
@@ -2,6 +2,7 @@
   $(function() {
     $('#nav-toggle').click(function(){
       $('#mobile-global-nav').toggleClass('open');
+      $('#mobile-global-nav').css('top', (scrollTop() + 67) + 'px');
       $('#nav-toggle').toggleClass('open');
     });
   });
