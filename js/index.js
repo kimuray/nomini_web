d329af870e013aaf2aaa7acf6ccb418e4a0b5e0f
Yoshihiro Kimura <kimura.yoshihiro@tsunolab.net>
2018-03-11 01:11:36 +0900

    スライダー作成

--- js/index.js
+++ js/index.js
@@ -6,4 +6,23 @@
       $('#nav-toggle').toggleClass('open');
     });
   });
+
+  $(function() {
+    var mySwiper = new Swiper ('.swiper-container', {
+      // Optional parameters
+      direction: 'horizontal',
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+      autoplay: {
+        delay: 3000,
+      },
+      pagination: {
+        el: '.swiper-pagination',
+        type: 'bullets',
+      },
+      slidesPerView: 1,
+    });
+  });
 })(jQuery);
